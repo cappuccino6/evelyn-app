fb7ba40543bb230c18fbb720a27a3f26767927bb
zhengyuanbing <zhengyuanbing.007>
2024-03-18 16:52:51 +0800

    feat: init

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const element = document.getElementById('root');
+// element?.setAttribute?.('class', 'w-full h-full')
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
